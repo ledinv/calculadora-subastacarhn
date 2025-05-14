fbd4255cd865ad6ed8b6e12271048095cb9b2b69
ledinv <ledinvvv@gmail.com>
2025-05-14 02:24:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ function calcular() {
     c16 = 0; // PICK UP nunca paga ISC
   } else if (c14 === "HIBRIDO") {
     c16 = (c11 + o3 + o4 + c15) * 0.05;
-  } else if (["CAMION", "BUS", "MAQUINARIA"].includes(c14)) {
+  } else if (["CAMION", "BUS", "MAQUINARIA", "MOTO"].includes(c14)) {
     c16 = 0;
   } else {
     const baseISCusd = c10 + o3usd + o4usd;
